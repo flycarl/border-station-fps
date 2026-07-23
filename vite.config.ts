26782056496f9b73a193944f43efb2620ca3ebeb
flycarl <652347+flycarl@users.noreply.github.com>
2026-07-23 10:07:28 +0800

    fix: use relative asset paths for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,5 @@
 import { defineConfig } from 'vite';
-export default defineConfig({ build: { target: 'es2022', sourcemap: true } });
+export default defineConfig({
+  base: './',
+  build: { target: 'es2022', sourcemap: true },
+});
